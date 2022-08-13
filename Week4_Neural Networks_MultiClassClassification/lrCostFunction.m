298b9be6701b90371bba91f39463c340607ec7e0
Jonathan Hasan <jonh.hasan@gmail.com>
2022-08-13 00:27:08 -0500

    Completed Week 4- Neural Networks MultiClass Classification Project
    Finished files for LrCostFunction.m, oneVsAll.m, predictOneVsAll.m and predict.m. Implements cost function and gradient, theta parameter modification, feedforward operation and finally the class predictions.

--- Week4_Neural Networks_MultiClassClassification/lrCostFunction.m
+++ Week4_Neural Networks_MultiClassClassification/lrCostFunction.m
@@ -1,14 +1,14 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -25,23 +25,43 @@ grad = zeros(size(theta));
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
-% Hint: When computing the gradient of the regularized cost function, 
+% Hint: When computing the gradient of the regularized cost function,
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+% Unregularized function and grad
+hypothesis = sigmoid(X * theta);
 
+cost_function_zero = -y' * log(hypothesis);
 
+cost_function_one = (1-y)' * log(1 - hypothesis);
 
+J = (1/m) * sum(cost_function_zero - cost_function_one);
 
+grad =  (1/m) * X' * (sigmoid(X* theta) - y);
 
+% only apply reg terms to rows that are not zeroth row.
 
+temp = theta;
+
+temp(1) = 0;
+
+reg_grad = (lambda/m)* temp;
+
+reg_J = (lambda/(2*m))*sum(theta' * temp);
+
+%Results after reg term uses modified theta values
+
+J = J + reg_J;
+
+grad = grad + reg_grad;
 
 
 
--- Week4_Neural Networks_MultiClassClassification/predict.m
+++ Week4_Neural Networks_MultiClassClassification/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -20,6 +20,32 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+
+%feedforward operation time. Perform activations between each layer and determine which row is which class
+
+% Need to add a bias unit to transform into activation function layer 1
+
+column_ones = ones(size(X,1),1);
+
+a1 = [column_ones,X];
+
+%Multiply by Theta 1 and get 'z2'
+
+a2 = sigmoid(a1 * Theta1');
+
+%apply activation function and add ones to turn into activation layer 2
+
+a2 = [ones(size(a2,1),1), a2];
+
+%Multiply the activation layer 2 by Theta 2, compute sigmoid and make a3
+
+a3 = sigmoid(a2*Theta2');
+
+%Acquire the vectors, first vector shows indices of max values and second is vector of predictions for each row in X.
+
+[index p] = max(a3, [], 2);
+
+
 
 
 
